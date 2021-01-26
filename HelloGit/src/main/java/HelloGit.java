6dc601977d976d2af29881e638c4824b745e2053
Chen-1026 <1223325118@qq.com>
2021-01-26 14:23:18 +0800

    主干添加

--- HelloGit/src/main/java/HelloGit.java
+++ HelloGit/src/main/java/HelloGit.java
@@ -3,6 +3,6 @@ public class HelloGit {
         System.out.println("helloGit");
         System.out.println("更新一");
         System.out.println("更新二");
-        System.out.println("创建分支");
+        System.out.println("主干添加");
     }
 }
