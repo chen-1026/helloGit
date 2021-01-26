49bf0a9ed53b12142313d2e93a43bc56c36c7c1c
Chen-1026 <1223325118@qq.com>
2021-01-26 14:06:24 +0800

    更新二

--- HelloGit/src/main/java/HelloGit.java
+++ HelloGit/src/main/java/HelloGit.java
@@ -2,5 +2,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("helloGit");
         System.out.println("更新一");
+        System.out.println("更新二");
     }
 }
